d56b08e69a15facd58a8ca986aeaf86ff937f0fe
Willian <willian2o12@hotmail.com>
2021-04-21 18:06:24 -0300

    :sparkles: Day 1

---
+++ lib/core/app_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import 'package:DevQuiz/home/home_page.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "DevQuiz",
+      home: HomePage(),
+    );
+  }
+}
---
+++ lib/splash/splash_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import '../core/app_gradients.dart';
+import '../core/app_images.dart';
+
+class SplashPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(gradient: AppGradients.linear),
+        child: Center(
+          child: Image.asset(AppImages.logo),
+        ),
+      ),
+    );
+  }
+}
